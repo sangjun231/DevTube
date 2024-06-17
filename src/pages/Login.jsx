b71a1f4e7554005d287c6f0fdafe6c0ec70f03f0
lyuna29 <hyunah0418@gmail.com>
2024-06-18 01:56:02 +0900

    Design:Login UI

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,14 +1,19 @@
-import React from 'react';
+import React from 'react'
+import logo from '../assets/Devtube_logo.png'
 
 const Login = () => {
   return (
-    <div className="bg-yellow-50">
-      <h2>로그인</h2>
-      <div className="bucket">
-        <input placeholder="아이디" />
-        <input placeholder="비밀번호" />
-        <button type="button">로그인</button>
-        <button type="button">회원가입</button>
+    <div className="flex justify-center items-center">
+      <div className="bg-yellow-50 w-80 h-96 flex flex-col justify-center items-center"  > 
+        <img className="w-24" src={logo} alt="로고" />
+        <div className="flex flex-col justify-center items-center">
+          <input className="w-56 m-3" placeholder="아이디" />
+          <input className="w-56 m-3" placeholder="비밀번호" />
+          <div className="m-2">
+            <button className="m-2 border-solid border-slate-300" type="button">로그인</button>
+            <button className="m-2" type="button">회원가입</button>
+          </div>
+        </div>
       </div>
     </div>
   );
--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -1,10 +1,15 @@
 import { createBrowserRouter } from 'react-router-dom';
 import Auth from '../pages/Auth';
+import Login from '../pages/Login';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <Auth />
+  },
+  {
+    path: '/login',
+    element: <Login />
   }
 ]);
 
