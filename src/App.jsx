9276d7d08117d3749fc47e3243c3df7689ad5b01
JJQKR <yjcoyjco@naver.com>
2024-06-17 23:14:56 +0900

    Feat : fake data 불러오기

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,42 @@
+import MainPage from './pages/MainPage';
+import { useQuery } from '@tanstack/react-query';
+import axios from 'axios';
+
+const getTest = async () => {
+  const response = await axios.get('http://localhost:4000/test');
+  return response.data;
+};
+
+const getTestById = async () => {
+  const response = await axios.get(`http://localhost:4000/test/${id}`);
+  return response.data;
+};
+
 function App() {
-  return <></>;
+  const {
+    data: testData,
+    isPending,
+    isError
+  } = useQuery({
+    queryKey: ['test'],
+    queryFn: getTest
+  });
+
+  if (isPending) {
+    return <div>Loading..</div>;
+  }
+
+  if (isError) {
+    return <div>Error occured while fetching data.</div>;
+  }
+
+  return (
+    <>
+      {testData.map((item) => (
+        <div key={item.id}>{item.title}</div>
+      ))}
+    </>
+  );
 }
 
 export default App;
--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,16 +1,5 @@
-import axios from 'axios';
 import React from 'react';
 
 export default function MainPage() {
-  const getTest = async () => {
-    const response = await axios.get('http://localhost:/4000/test');
-    return response.data;
-  };
-
-  const getTestById = async (id) => {
-    const response = await axios.get(`http://localhost:4000/test/${id}`);
-    return response.data;
-  };
-
   return <div>MainPage</div>;
 }
