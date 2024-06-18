083931f21ce284dc20ef414ba5e1fb730dae0c69
sangjunShin <987654321123sin@gmail.com>
2024-06-19 00:32:55 +0900

    Feat: 전체 페이지 toast적용 가능하게 세팅 , 마이페이지 , 메인페이지 기능 추가 및 개선
    검색 시 DB에 저장 되지 않고, 저장 시에만 디비에 저장되며 일정 시간동안 toast 메시지와 함께 연속 저장 방지 , 저장된 영상은 마이페이지에서 확인 가능하며 삭제 가능

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
-
 import QueryClientSetup from './QueryClientSetup.jsx';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <QueryClientSetup>
+      <ToastContainer />
       <ReactQueryDevtools initialIsOpen={false} />
       <App />
     </QueryClientSetup>
