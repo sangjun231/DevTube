2e18ea67fef5da1bab3d54cd12633a6b4da58dcf
sangjunShin <987654321123sin@gmail.com>
2024-06-18 18:12:16 +0900

    Feat: 유튜브API 검색 조건으로 랜더링 후 DB 저장까지 구현

--- src/QueryClientSetup.jsx
+++ src/QueryClientSetup.jsx
@@ -1,6 +1,7 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { Children } from 'react';
+
 const queryClient = new QueryClient();
+
 const QueryClientSetup = ({ children }) => {
   return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>;
 };
--- src/lib/supabase/supabase.js
+++ src/lib/supabase/supabase.js
@@ -1,4 +1,5 @@
 import { createClient } from '@supabase/supabase-js';
-const supabaseUrl = import.meta.env.VITE_PROJECT_URL;
-const supabaseKey = import.meta.env.VITE_API_KEY;
+
+const supabaseUrl = import.meta.env.VITE_SUPABASE_PROJECT_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_API_KEY;
 export const supabase = createClient(supabaseUrl, supabaseKey);
