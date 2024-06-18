1894c5075c59327971d4388562ee4704f4618888
lyuna29 <hyunah0418@gmail.com>
2024-06-18 14:28:31 +0900

    Feat:supabase 연결 전 코드 작성

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,11 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        bgDev: '#FFFFF0'
+      }
+    }
   },
   plugins: []
 };
