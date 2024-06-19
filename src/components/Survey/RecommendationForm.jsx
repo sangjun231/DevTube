d7fd35f44212464bbc02b99ee4b391218ec5fbd8
hyeonw8 <whgusrud223@gmail.com>
2024-06-19 09:30:23 +0900

    fix : onSubmitSurvey (제출 이벤트) 수정

--- src/components/Survey/RecommendationForm.jsx
+++ src/components/Survey/RecommendationForm.jsx
@@ -1,5 +1,7 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { surveyApi } from '../../lib/supabase/surveyApi';
+import { toast } from 'react-toastify';
 
 const RecommendationForm = ({ answers, setAnswers, onNext, setStep }) => {
   // const [level, setLevel] = useState('');
@@ -25,31 +27,70 @@ const RecommendationForm = ({ answers, setAnswers, onNext, setStep }) => {
     //console.log(answers)
   };
 
-  const onSubmit = (e) => {
-    e.preventDefault();
+  // const onSubmit = (e) => {
+  //   e.preventDefault();
 
+  //   if (!answers.level) {
+  //     alert('추천받고 싶은 영상의 난이도를 선택해주세요');
+  //     return;
+  //   }
+
+  //   if(answers.topics.length === 0) {
+  //     alert('추천받고 싶은 영상의 키워드를 선택해주세요');
+  //     return;
+  //   }
+  //   localStorage.setItem('answers', JSON.stringify(answers));
+
+  //   onNext(answers);
+  // };
+
+  const onSubmitSurvey = async (e) => {
+    e.preventDefault();
+  
     if (!answers.level) {
       alert('추천받고 싶은 영상의 난이도를 선택해주세요');
       return;
     }
-
-    if(answers.topics.length === 0) {
+  
+    if (answers.topics.length === 0) {
       alert('추천받고 싶은 영상의 키워드를 선택해주세요');
       return;
     }
-
-    onNext(answers);
+  
+    setAnswers({ ...answers });
+    console.log('제출할 데이터 준비', answers);
+  
+    try {
+      const userAnswer = await surveyApi(answers);
+      console.log('제출용 answers', answers);
+      toast.success('답변이 제출되었습니다.');
+      // localStorage.setItem('answers', JSON.stringify(answers));
+  
+      onNext(answers);
+    } catch (e) {
+      console.error('Error submitting survey:', e.message);
+      alert('답변 제출에 실패했습니다. 다시 시도해 주세요.');
+      setStep('사전배경입력');
+    }
   };
 
+  // const savedAnswers = JSON.parse(localStorage.getItem('answers'));
+
+  // useEffect(() => {
+  //   if (savedAnswers) {
+  //     setAnswers(savedAnswers);
+  //   }
+  // }, []); 
+
   return (
-    <div className="flex h-full flex-col items-center justify-center">
+    <div className="flex flex-col items-center justify-center">
       <div className="w-[500px] rounded-lg bg-slate-200 p-8 shadow-lg">
         <h1 className="mb-4 text-left text-xl font-semibold">안녕하세요! 😊</h1>
         <p className="mb-6 text-left">
           몇가지 간단한 질문에 답변해 주시면, <br />
           알맞는 영상을 추천해 드릴게요!
         </p>
-        <form onSubmit={onSubmit}>
+        <form onSubmit={onSubmitSurvey}>
           <div className="mb-4 flex flex-col rounded-2xl border-2 border-solid border-gray-400 p-4">
             <label className="mb-2 block font-medium">추천받고 싶은 영상의 난이도를 선택해주세요.</label>
             <div className="flex justify-end space-x-4">
@@ -104,7 +145,7 @@ const RecommendationForm = ({ answers, setAnswers, onNext, setStep }) => {
           </button>
         </form>
       </div>
-      <button className="mt-10 underline" onClick={() => setStep('사전배경입력')}>
+      <button className="mt-8 underline" onClick={() => setStep('사전배경입력')}>
         이전으로
       </button>
     </div>
