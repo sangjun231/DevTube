5f3a48e37f1e2f7a23fd26c3a25e0f6eded275a7
hyeonw8 <whgusrud223@gmail.com>
2024-06-19 09:32:12 +0900

    remove :  기존 설문조사 api 파일 삭제

--- src/lib/api/supabase/postUserSurveyAPI.js
+++
@@ -1,8 +0,0 @@
-const SUPABASE_PROJECT_URL = import.meta.env.VITE_SUPABASE_URL;
-const SUPABASE_ANON_KEY = import.meta.env.VITE_SUPABASE_KEY;
-
-export const supabase = createClient(SUPABASE_PROJECT_URL, SUPABASE_ANON_KEY);
-
-export const postUserSurveyAPI = async (answers) => {
-  
-};
