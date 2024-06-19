43b4d724136c5946784eab08fd1a26d2d6e07cab
hyeonw8 <whgusrud223@gmail.com>
2024-06-19 21:01:15 +0900

    fix :  설문조사 내용 변경 및 value 값 수정,  css 추가 수정

--- src/components/Survey/SurveyForm.jsx
+++ src/components/Survey/SurveyForm.jsx
@@ -1,16 +1,6 @@
-import React, { useEffect } from 'react';
-import { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { toast } from "react-toastify";
 
 const SurveyForm = ({ answers, setAnswers, onNext }) => {
-  // const [answers, setAnswers] = useState({
-  //   isMajor: '',
-  //   hasFrontendExperience: '',
-  //   usedReact: '',
-  //   usedZustand: '',
-  // });
-
-  //const navigate = useNavigate();
 
   const onChange = (e) => {
     const { name, value } = e.target;
@@ -23,23 +13,19 @@ const SurveyForm = ({ answers, setAnswers, onNext }) => {
   const onSubmit = (e) => {
     e.preventDefault();
 
-    if(!answers.isMajor || !answers.hasFrontendExperience 
-      || !answers.usedReact || !answers.usedZustand) {
-      alert('모든 질문에 대답해 주세요!');
+    if(!answers.isMajor || !answers.level ) {
+      toast.error('모든 질문에 대답해 주세요!');
       return;
     }
     localStorage.setItem('answers', JSON.stringify(answers));
 
-    
-    //console.log(answers);
     onNext(answers);
   };
 
-  
 
   return (
-    <div className="flex items-center justify-center">
-      <div className="w-[500px] rounded-lg bg-slate-200 p-8 shadow-lg">
+    <div className="flex items-center justify-center mt-20">
+      <div className="w-[550px] rounded-lg bg-slate-200 p-8 shadow-lg">
         <h1 className="mb-4 text-left text-xl font-semibold">안녕하세요! 😊</h1>
         <p className="mb-6 text-left ">
           몇가지 간단한 질문에 답변해 주시면, <br />
@@ -47,118 +33,78 @@ const SurveyForm = ({ answers, setAnswers, onNext }) => {
         </p>
         <form onSubmit={onSubmit}>
           <div className="mb-4 flex items-center border-solid border-2 border-gray-400 p-4 rounded-2xl justify-between">
-            <label className="mr-4 block font-medium">전공자이신가요?</label>
+            <label className="mr-4 block font-medium text-lg">전공자이신가요?</label>
             <div className="flex space-x-4">
-              <label>
+              <label className='text-lg'>
                 <input 
                   type="radio" 
                   name="isMajor" 
-                  value="yes" 
+                  value="전공자" 
                   onChange={onChange} 
                   className="mr-2"
-                  checked={answers.isMajor === 'yes'}
-                  required
+                  checked={answers.isMajor === '전공자'}
                 />
                   예
               </label>
-              <label>
+              <label className='text-lg'>
                 <input 
                   type="radio" 
                   name="isMajor" 
-                  value="no" 
+                  value="비전공자" 
                   onChange={onChange} 
                   className="mr-2" 
-                  checked={answers.isMajor === 'no'}
-                  required
+                  checked={answers.isMajor === '비전공자'}
                 />
                 아니요
               </label>
             </div>
           </div>
-          <div className="mb-4 flex items-center border-solid border-2 border-gray-400 p-4 rounded-2xl justify-between">
-            <label className="mr-4 block font-medium">웹 개발 프론트엔드 경험이 있으신가요?</label>
-            <div className="flex space-x-4">
-              <label>
+          <div className="mb-4 flex flex-col  border-solid border-2 border-gray-400 p-4 rounded-2xl justify-between">
+            <label className="mr-4 block font-medium items-left mb-4 text-lg ">본인의 수준을 골라주세요</label>
+            <div className="flex space-x-4 justify-end">
+            <label className='text-lg'>
                 <input 
                   type="radio" 
-                  name="hasFrontendExperience" 
-                  value="yes" 
+                  name="level" 
+                  value="입문" 
                   onChange={onChange} 
                   className="mr-2" 
-                  required
-                  checked={answers.hasFrontendExperience === 'yes'}
+                  checked={answers.level === '입문'}
                   />
-                  예
+                  입문
               </label>
-              <label>
+              <label className='text-lg'>
                 <input 
                   type="radio" 
-                  name="hasFrontendExperience" 
-                  value="no" 
+                  name="level" 
+                  value="초급" 
                   onChange={onChange} 
                   className="mr-2" 
-                  required
-                  checked={answers.hasFrontendExperience === 'no'}
+                  checked={answers.level === '초급'}
                 />
-                아니요
+                초급
               </label>
-            </div>
-          </div>
-          <div className="mb-4 flex items-center border-solid border-2 border-gray-400 p-4 rounded-2xl justify-between">
-            <label className="mr-4 block font-medium">리액트를 사용해본 적이 있으신가요?</label>
-            <div className="flex space-x-4 ">
-              <label>
+              <label className='text-lg'>
                 <input 
                   type="radio" 
-                  name="usedReact" 
-                  value="yes" 
+                  name="level" 
+                  value="중급" 
                   onChange={onChange} 
                   className="mr-2" 
-                  required
-                  checked={answers.usedReact === 'yes'}
+                  checked={answers.level === '중급'}
                 />
-                  예
+                중급
               </label>
-              <label>
+              <label className='text-lg'>
                 <input 
                   type="radio" 
-                  name="usedReact" 
-                  value="no" 
+                  name="level" 
+                  value="고급" 
                   onChange={onChange} 
                   className="mr-2" 
-                  required
-                  checked={answers.usedReact === 'no'}
+                  checked={answers.level === '고급'}
                 />
-                아니요
-              </label>
-            </div>
-          </div>
-          <div className="mb-6 flex items-center border-solid border-2 border-gray-400 p-4 rounded-2xl justify-between">
-            <label className="mr-4 block font-medium">주스탠드를 써보셨나요?</label>
-            <div className="flex space-x-4">
-              <label>
-                <input 
-                  type="radio" 
-                  name="usedZustand" 
-                  value="yes" 
-                  onChange={onChange} 
-                  className="mr-2" 
-                  required
-                  checked={answers.usedZustand === 'yes'}
-                />
-                예
-              </label>
-              <label>
-                <input 
-                  type="radio" 
-                  name="usedZustand" 
-                  value="no" 
-                  onChange={onChange} 
-                  className="mr-2"
-                  required
-                  checked={answers.usedZustand === 'no'}
-                />
-                아니요
+                고급
               </label>
             </div>
           </div>
