f9ef94fd118f0b56d44ae760db06d7ecbe76ad56
hyeonw8 <whgusrud223@gmail.com>
2024-06-19 09:37:01 +0900

    fix : 사용자 정보에 userId 추가 및 제출 로직 이동, 설문조사 페이지 진입 시 userId 받아오도록 추가

--- src/components/Survey/QuestionForm.jsx
+++ src/components/Survey/QuestionForm.jsx
@@ -1,12 +1,13 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import SurveyForm from './SurveyForm';
 import RecommendationForm from './RecommendationForm';
 import AnswerSubmit from './AnswerSubmit';
+import {  getUserApi, surveyApi } from '../../lib/supabase/surveyApi';
 
 const QuestionForm = () => {
-  // 유저id 받아와서 answers에 값을 같이 넣어줘야 하나?
   const [answers, setAnswers] = useState({
+    userId: '',
     isMajor: '',
     hasFrontendExperience: '',
     usedReact: '',
@@ -15,9 +16,8 @@ const QuestionForm = () => {
     topics: []
   });
 
-
   const [step, setStep] = useState('사전배경입력');
-  const navigate = useNavigate();
+  // const userId = '32c50ec5-7438-4735-850f-d3acf8a24190';
 
   const onNextSurvey = (data) => {
     setAnswers({ ...answers, ...data });
@@ -28,19 +28,83 @@ const QuestionForm = () => {
     setAnswers({ ...answers, ...data });
     setStep('답변제출');
   };
+  
+  // useEffect(() => {
+  //   const getData = async () => {
+  //     try {
+  //       const userId = '32c50ec5-7438-4735-850f-d3acf8a24193';
+  //       const getUserId = await getUserApi(userId);
+  //       console.log('받아온 유저 아이디 확인', userData);
+  //       setAnswers({...answers, userId : userData});
+  //       console.log('아이디 포함 저장 데이터', answers);
+  //       //if (userId) {
+  //         //const selection = user.selection || {};
 
-  const onSumbitSurvey = (data) => {
-    setAnswers({ ...answers, ...data });
-    //db 제출 로직 추가
-    navigate('/');
-  };
+  //         // setAnswers({
+  //         //   userId: user.id,
+  //         //   isMajor: selection.isMajor || '',
+  //         //   hasFrontendExperience: selection.hasFrontendExperience || '',
+  //         //   usedReact: selection.usedReact || '',
+  //         //   usedZustand: selection.usedZustand || '',
+  //         //   level: selection.level || '',
+  //         //   topics: selection.topics || []
+  //         // });
+
+  //         console.log('저장한 상태 데이터', answers);
+  //      // }
+  //     } catch (e) {
+  //       console.log('데이터 받아오기 오류', e.message);
+  //     }
+  //   }
+  //   getData();
+  // }, []);
+
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const userId = 'faaa3839-18ee-4064-87f4-9bdc994b4bde'; 
+        const userData = await getUserApi(userId);
+        console.log('userData 값 확인',userData)
+  
+        if (userData) {
+          const selection = {};
+
+          setAnswers({
+            userId: userData,
+            isMajor: selection.isMajor || '',
+            hasFrontendExperience: selection.hasFrontendExperience || '',
+            usedReact: selection.usedReact || '',
+            usedZustand: selection.usedZustand || '',
+            level: selection.level || '',
+            topics: selection.topics || []
+          });
+  
+          console.log('저장한 상태 데이터(QuestionForm)', {
+            userId: userData,
+            isMajor: selection.isMajor || '',
+            hasFrontendExperience: selection.hasFrontendExperience || '',
+            usedReact: selection.usedReact || '',
+            usedZustand: selection.usedZustand || '',
+            level: selection.level || '',
+            topics: selection.topics || []
+          });
+        }
+      } catch (e) {
+        console.log('데이터 받아오기 오류', e.message);
+      }
+    };
+  
+    getData();
+  }, []);
 
   return (
-    <div className='flex justify-center items-center h-full mt-20'>
+    <div className="flex items-center justify-center mt-6">
       {/* 상태관리로 변경 */}
-      {step === '사전배경입력' && <SurveyForm onNext={onNextSurvey} answers={answers} setAnswers={setAnswers}/>}
-      {step === '요구사항입력' && <RecommendationForm onNext={onRecommendationNext} setStep={setStep} answers={answers} setAnswers={setAnswers}/>}
-      {step === '답변제출' && <AnswerSubmit onNext={onSumbitSurvey} setStep={setStep}  answers={answers}/>}
+      {step === '사전배경입력' && <SurveyForm onNext={onNextSurvey} answers={answers} setAnswers={setAnswers} />}
+      {step === '요구사항입력' && (
+        <RecommendationForm onNext={onRecommendationNext} setStep={setStep} answers={answers} setAnswers={setAnswers} />
+      )}
+      {step === '답변제출' && <AnswerSubmit setStep={setStep} answers={answers} />}
     </div>
   );
 };
