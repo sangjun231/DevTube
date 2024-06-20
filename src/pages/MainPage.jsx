a403e0dff67873c4fb5e0e9f4460d881c106cc8a
JJQKR <yjcoyjco@naver.com>
2024-06-20 16:02:05 +0900

    Feat : 설문 기반 추천영상 구현 중

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,11 +1,12 @@
 import { useEffect, useState } from 'react';
-import { getUserSelection } from '../lib/supabase/userSelctionApi';
-//같은 로직인데 setUserSelection부분이 없음
-//안 쓰이는 중
 import { supabase } from '../lib/supabase/supabase';
 
+import { searchYouTubeVideos } from '../lib/api/youtubeAPI';
+
 const MainPage = () => {
-  const [userSelection, setUserSelection] = useState(null);
+  // const [userSelection, setUserSelection] = useState(null);
+  const [selectionQuery, setSelectionQuery] = useState('');
+  const [selectionVideos, setSelectionVideos] = useState([]);
 
   useEffect(() => {
     const fetchUserSelection = async () => {
@@ -21,7 +22,9 @@ const MainPage = () => {
           throw error;
         }
         console.log(data);
-        setUserSelection(data);
+        const query = `${data.selection.level} ${data.selection.topics}`;
+        setSelectionQuery(query);
+        console.log(query);
       } catch (error) {
         console.error('Error in fetchUserSelection:', error);
       }
@@ -30,7 +33,44 @@ const MainPage = () => {
     fetchUserSelection();
   }, []);
 
-  return <>돼라</>;
+  //query안에 data.selection.level + data.selection.topics
+
+  useEffect(() => {
+    const renderSelectionVideos = async () => {
+      try {
+        const selectionVideos = await searchYouTubeVideos(selectionQuery);
+        setSelectionVideos(
+          selectionVideos.map((video) => ({
+            video_title: video.snippet.title,
+            video_id: video.id.videoId
+          }))
+        );
+      } catch (error) {
+        console.error('Error fetching data from YouTube API:', error);
+      }
+    };
+    renderSelectionVideos();
+  }, []);
+
+  return (
+    <>
+      <div className="grid grid-cols-3 gap-4">
+        {selectionVideos.map((video) => (
+          <div key={video.video_id}>
+            <h3 dangerouslySetInnerHTML={{ __html: video.video_title }} className="w-full truncate"></h3>
+            <div className="aspect-h-9 aspect-w-16">
+              <iframe
+                className="h-full w-full"
+                src={`https://www.youtube.com/embed/${video.video_id}`}
+                allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+                allowFullScreen
+              ></iframe>
+            </div>
+          </div>
+        ))}
+      </div>
+    </>
+  );
 };
 
 export default MainPage;
